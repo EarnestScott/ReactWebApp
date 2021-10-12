86bb672a5d34f32ded8c411f58f8dd8c0b90be6f
Earnest Scott <earnestscott@Earnests-MacBook-Air.local>
2021-10-11 20:31:58 -0700

    ejected app and created NewsDisplay component

--- src/NewsDisplay.js
+++ src/NewsDisplay.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import bluebird from 'bluebird';
 import Card from 'react-bootstrap/Card';
-import fs from 'fs';
-const rawData = fs.readFileSync('../config.json');
-const config = JSON.parse(rawData);
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// import * as fs from 'fs';
+// const rawData = fs.readFileSync('../config.json');
+// const config = JSON.parse(rawData);
 class NewsDisplay extends Component {
     constructor(props) {
         super(props);
@@ -13,12 +17,12 @@ class NewsDisplay extends Component {
     }
 
     async componentDidMount() {
-        this.setState({ companyData: await this.getCompanyData() });
+        // this.setState({ companyData: await this.getCompanyData() });
     }
     async getCompanyData() {
         const companies = ['tesla', 'apple', 'qualcomm'];
         const companyData = await bluebird.mapSeries(companies, async company => {
-            const response = await fetch(`https://newsapi.org/v2/everything?q=${company}&from=2021-10-10&sortBy=popularity&pageSize=1&apiKey=${config.newsApiKey}`)
+            const response = await fetch(`https://newsapi.org/v2/everything?q=${company}&from=2021-10-10&sortBy=popularity&pageSize=1&apiKey=`)
             const responseJson = await response.json();
             return responseJson;
         });
@@ -26,12 +30,14 @@ class NewsDisplay extends Component {
         console.log(cleanedCompanyData);
         return cleanedCompanyData;
     }
+
     render() {
+
         const { companyData } = this.state;
-        return (
-            <div>
-                {companyData && companyData.map(({ title, description, urlToImage }) => <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" style={{ width: 50, height: 50 }} src={urlToImage} />
+        const displayCompanyData = companyData?.map(({ title, description, urlToImage }) =>
+            <Col>
+                <Card className={"mb-2"} border={"dark"} bg={"light"} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={urlToImage} />
                     <Card.Body>
                         <Card.Title>{title}</Card.Title>
                         <Card.Text>
@@ -39,8 +45,17 @@ class NewsDisplay extends Component {
                         </Card.Text>
                         {/* <Button variant="primary">Go somewhere</Button> */}
                     </Card.Body>
-                </Card>)}
-            </div>
+                </Card>
+            </Col>)
+        return (
+            <React.Fragment>
+                <Container>
+                    {companyData && <Row>
+                        {displayCompanyData}
+                    </Row>}
+                </Container>
+
+            </React.Fragment>
 
         );
     }
