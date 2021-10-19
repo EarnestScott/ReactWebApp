42deabccf4529695aab6b808287dda8fd87c982d
Earnest Scott <earnestscott@Earnests-Air.lan>
2021-10-18 18:20:46 -0700

    forgot to add this

---
+++ src/FriendHome.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function FriendHome() {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const fetchFriends = await fetch('http://localhost:3001/friends', {
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const fetchFriendsJson = await fetchFriends.json();
+            console.log(fetchFriendsJson);
+            // assuming it's an array
+            if (fetchFriendsJson.length) {
+                setFriends(fetchFriendsJson);
+            }
+        }
+        if (friends.length === 0) fetchData();
+    });
+    return (
+        <>
+            <h1>
+                Friends
+            </h1>
+            <div>
+                {friends && friends.map(({ firstName, lastName }) => <Card>
+                    <Card.Body>
+                        <Card.Title>{firstName} {lastName}</Card.Title>
+                        <Card.Text>
+                            Friends
+                        </Card.Text>
+                    </Card.Body>
+                </Card>)}
+
+            </div>
+        </>
+    );
+}
+
+export default FriendHome;
